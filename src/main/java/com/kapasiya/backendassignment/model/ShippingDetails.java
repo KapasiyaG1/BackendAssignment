b48ff659821529d89822c902233f5549f53a66b0
KapasiyaG1 <soyamp232@gmail.com>
2023-12-27 17:23:40 +0530

    creating ShippingDetailsRepository

--- src/main/java/com/kapasiya/backendassignment/model/ShippingDetails.java
+++ src/main/java/com/kapasiya/backendassignment/model/ShippingDetails.java
@@ -21,4 +21,66 @@ public class ShippingDetails {
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
+
+
+    public ShippingDetails()
+    {
+
+    }
+
+    public ShippingDetails(String address, String city, String pincode, PurchaseOrder purchaseOrder, Customer customer) {
+        this.address = address;
+        this.city = city;
+        this.pincode = pincode;
+        this.purchaseOrder = purchaseOrder;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public PurchaseOrder getPurchaseOrder() {
+        return purchaseOrder;
+    }
+
+    public void setPurchaseOrder(PurchaseOrder purchaseOrder) {
+        this.purchaseOrder = purchaseOrder;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
---
+++ src/main/java/com/kapasiya/backendassignment/repository/ShippingDetailsRepository.java
@@ -0,0 +1,7 @@
+package com.kapasiya.backendassignment.repository;
+
+import com.kapasiya.backendassignment.model.ShippingDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShippingDetailsRepository extends JpaRepository<ShippingDetails, Long> {
+}
